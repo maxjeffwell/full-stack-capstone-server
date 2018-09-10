3e7c6290c6a5ab1a0edc9972940df72aea25e410
Jeff Maxwell <maxjeffwell@gmail.com>
2018-09-10 16:00:43 -0400

    added authenticated signup route

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
-// We're going to export a function from this file, import it into index.js, and then pass app into that function
+const Authentication = require('./controllers/authentication');
+
 
 module.exports = function(app) { // Inside this function we have access to our Express app
-    app.get('/', function(req, res, next) { // Define a route that the user can visit
-     res.send(['grades', 'age', 'status']);
-    });
+    app.post('/signup', Authentication.signup); // Define a route that the user can visit
+
 }
