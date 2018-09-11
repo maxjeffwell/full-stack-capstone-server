fed008a8a509907a8b0f9c9af5a702e27ce88b0b
Jeff Maxwell <maxjeffwell@gmail.com>
2018-09-10 22:10:47 -0400

    created signin route

--- router.js
+++ router.js
@@ -1,12 +1,17 @@
 const Authentication = require('./controllers/authentication');
-// const passportConfig = require('./services/passport');
+const passportConfig = require('./services/passport');
 const passport = require('passport');
 
 // Create an object and insert it between our incoming request and our route handler (i.e. Passport middleware - requireAuth)
 
-// const requireAuth = passport.authenticate('jwt', { session: false }); // When a user is authenticated don't try to make a session for them (by default, Passport tries to make a cookie-based session for the request, but we're using tokens and we don't want that)
+const requireAuth = passport.authenticate('jwt', { session: false }); // When a user is authenticated don't try to create a session for them (by default, Passport tries to make a cookie-based session for the request, but we're using tokens and we don't want that)
+
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) { // Inside this function we have access to our Express app
-    app.post('/signup', Authentication.signup); // Define a route that the user can visit
-
+    app.get('/', requireAuth, function(req, res) {
+        res.send({ hello: 'you' });
+    });
+    app.post('/signin', requireSignin, Authentication.signin);
+    app.post('/signup', Authentication.signup);
 }
