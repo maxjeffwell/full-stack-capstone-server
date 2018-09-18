51fa9af8ba80c571d044009c46bc00abf7a05bcd
Jeff Maxwell <maxjeffwell@gmail.com>
2018-09-17 21:38:48 -0400

    add get all, get by id, and put functions

--- router.js
+++ router.js
@@ -1,31 +1,100 @@
 const Authentication = require('./controllers/authentication');
 const passport = require('passport');
-// const path = require('path');
 
 // Create an object and insert it between our incoming request and our route handler (i.e. Passport middleware - requireAuth)
 
-const requireAuth = passport.authenticate('jwt', { session: false }); // When a user is authenticated don't try to create a session for them (by default, Passport tries to make a cookie-based session for the request)
+const Student = require('./models/student');
+
+const requireAuth = passport.authenticate('jwt', { session: false }); // When a user is authenticated don't try to create a session for them (by default, Passport tries to make a cookie-based session for the request - we're using tokens)
 
 const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) { // Inside this function we have access to our Express app
-    app.get('/', requireAuth, function(req, res) {
+
+    app.get('/', requireAuth, function (req, res) {
         res.send('GET request to homepage');
     });
+
     app.post('/signin', requireSignin, Authentication.signin);
+
     app.post('/signup', Authentication.signup);
-
-
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.join(dirname+'../.client/build/index.html'));
-    // });
-
-    // app.get('/api/students', requireAuth,  {
-    //
-    // });
 
     app.get('/logout', (req, res) => {
         req.logout();
         res.redirect('/');
     });
+
+    app.get('/students', requireAuth, function (req, res) {
+        Student.find({})
+            .then((result) => {
+                res.json(result);
+            })
+            .catch((err) => {
+                res.status(500).json({success: false, msg: `Something didn't quite work right. ${err}`});
+            });
+    });
+
+    app.put('/students/:id', (req, res) => {
+
+        let updatedStudent = {
+            fullName: req.body.fullName,
+            school: req.body.school,
+            teacher: req.body.teacher,
+            gradeLevel: req.body.gradeLevel,
+            ellStatus: req.body.ellStatus,
+            compositeLevel: req.body.compositeLevel,
+            active: req.body.active,
+            designation: req.body.designation
+        };
+
+        Student.findOneAndUpdate({_id: req.params.id}, updatedStudent, {context: 'query'})
+            .then((oldResult) => {
+                Student.findOne({_id: req.params.id})
+                    .then((newResult) => {
+                        res.json({
+                            success: true,
+                            msg: `Updated successfully`,
+                            result: {
+                                fullName: newResult.fullName,
+                                school: newResult.school,
+                                teacher: newResult.teacher,
+                                gradeLevel: newResult.gradeLevel,
+                                ellStatus: newResult.ellStatus,
+                                compositeLevel: newResult.compositeLevel,
+                                active: newResult.active,
+                                designation: newResult.designation
+                            }
+                        });
+                    })
+
+                    .catch((err) => {
+                        console.error(err);
+                        res.status(500).json({success: false});
+                        return;
+                    });
+            })
+            .catch((err) => {
+                if (err.errors) {
+                    if (err.errors.school) {
+                        res.status(400).json({success: false, msg: err.errors.school.message});
+                        return;
+                    }
+                    if (err.errors.teacher) {
+                        res.status(400).json({success: false, msg: err.errors.teacher.message});
+                        return;
+                    }
+                    if (err.errors.gradeLevel) {
+                        res.status(400).json({success: false, msg: err.errors.gradeLevel.message});
+                        return;
+                    }
+                    if (err.errors.ellStatus) {
+                        res.status(400).json({success: false, msg: err.errors.ellStatus.message});
+
+                    }
+                }
+
+            });
+    });
 }
+
+
