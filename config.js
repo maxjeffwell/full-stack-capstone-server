12faf623cb0238e60c403b658fe33783f33c89a6
Jeff Maxwell <maxjeffwell@gmail.com>
2018-09-23 15:26:15 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 
 module.exports = {
     PORT: process.env.PORT || 8080,
-    CLIENT_ORIGIN: process.env.NODE_ENV.CLIENT_ORIGIN || 'http://localhost:3000',
+     CLIENT_ORIGIN: process.env.NODE_ENV === 'production' 
+    ? 'https://jmaxwell-fullstack-client.herokuapp.com' 
+    : 'http://localhost:3000',
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/local',
     TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost:27017/local',
     JWT_SECRET: process.env.JWT_SECRET,
