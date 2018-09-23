17a12ccb9a8ff9ea268daa04c4490e42993a9cac
Jeff Maxwell <maxjeffwell@gmail.com>
2018-09-23 16:57:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ app.use(bodyParser.json()); // Parses incoming requests into json
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization');
-  res.header('Access-Control-Allow-Methods', 'OPTIONS, GET,POST,PUT,PATCH,DELETE');
-//   if (req.method === 'OPTIONS') {
-//     return res.send(204);
-//   }
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
   next();
 });
 
