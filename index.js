8ad7d6a8f17f5ed897af921603ca5a0bca897f4b
Jeff Maxwell <maxjeffwell@gmail.com>
2018-09-23 18:41:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(bodyParser.json()); // Parses incoming requests into json
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
   if (req.method === 'OPTIONS') {
     return res.send(204);
