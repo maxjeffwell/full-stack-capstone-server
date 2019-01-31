0dbfeae31576f0c627f831e33468f040c0cb1a19
maxjeffwell <maxjeffwell@gmail.com>
2019-01-30 20:14:28 -0500

    make change to force re-deploy

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
-// Hold app secrets and config
-
 require('dotenv').config();
 
 module.exports = {
     PORT: process.env.PORT || 8080,
-     CLIENT_ORIGIN: process.env.NODE_ENV === 'production' 
-    ? 'https://jmaxwell-fullstack-client.herokuapp.com' 
+     CLIENT_ORIGIN: process.env.NODE_ENV === 'production'
+    ? 'https://jmaxwell-fullstack-client.herokuapp.com'
     : 'http://localhost:3000',
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/local',
     TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost:27017/local',
