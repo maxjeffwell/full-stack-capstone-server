c632591642c9793f357c614f35fff4fc15f69985
Jeff Maxwell <maxjeffwell@gmail.com>
2019-01-31 05:25:21 -0500

    delete config file and clean up router js

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ mongoose.set('useCreateIndex', true);
 // App setup to get Express working
 // Morgan and bodyParser are Express middleware (any incoming request will be passed into them by default)
 
-app.use(morgan('dev')); // Logging framework for logging incoming requests
-app.use(bodyParser.json()); // Parses incoming requests into json
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -52,12 +52,9 @@ if (process.env.NODE_ENV === 'production') {
 
     app.use(express.static('client/build'));
 
-
     // Express will serve up the index.html file if it doesn't recognize the route
 
-    // const path = require('path');
-
-    app.get('*', (req, res) => {
+  app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
